ae88b566c007c9fbbd6f39470c7ea8b488ee2604
Choilocal <choihyunji1103@gmail.com>
2021-07-15 14:35:22 +0900

    CORS proxy 추가

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/app/**", {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+      ws: true,
+    })
+  );
+};
